f10bd63bac30cad3bcf1199d72ba8a31d48e3408
Mel Panugaling <melpanugaling44@gmail.com>
2023-01-25 00:58:06 -0800

    final commit

--- src/app.js
+++ src/app.js
@@ -3,4 +3,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
+
+// Not found handler
+app.use((request, response, next) => {
+  next({ status: 404, message: `Not found: ${request.originalUrl}` });
+});
+
+// Error handler
+app.use((error, request, response, next) => {
+  console.error(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
+});
+
 module.exports = app;
