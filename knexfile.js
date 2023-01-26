7522cd8ca217f2fcf16c219d2084e6621f46dbf7
Mel Panugaling <melpanugaling44@gmail.com>
2023-01-25 16:51:54 -0800

    fixed database_url in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ const {
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: process.env.DEVELOPMENT_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
